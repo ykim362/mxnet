7a58b25a1a7ca13bdcde83cefd23f16e03e568b0
Ashok Emani <ashok.emani@intel.com>
2017-11-02 09:24:27 -0700

    added to fused-rnn mkl tests rnn-tanh, bidirectional, residual and unfuse

--- tests/python/mkl/test_operator_mkldnn.py
+++ tests/python/mkl/test_operator_mkldnn.py
@@ -55,6 +55,16 @@ def test_fused_rnn():
     check_rnn_consistency(fused, stack)
     check_rnn_consistency(stack, fused)
 
+def test_fused_rnn_tanh():
+    fused = mx.rnn.FusedRNNCell(100, num_layers=2, mode='rnn_tanh', prefix='')
+
+    stack = mx.rnn.SequentialRNNCell()
+    stack.add(mx.rnn.RNNCell(100, activation='tanh', prefix='l0_'))
+    stack.add(mx.rnn.RNNCell(100, activation='tanh', prefix='l1_'))
+
+    check_rnn_consistency(fused, stack)
+    check_rnn_consistency(stack, fused)
+
 
 def test_fused_lstm():
     fused = mx.rnn.FusedRNNCell(100, num_layers=2, mode='lstm', prefix='')
@@ -86,6 +96,55 @@ def test_fused_lstm_forget_bias():
     bias_name = next(x for x in args if x.endswith('f_bias'))
     expected_bias = forget_bias * np.ones(10, )
     assert_allclose(args[bias_name].asnumpy(), expected_bias)
+
+def test_residual_fused():
+    cell = mx.rnn.ResidualCell(
+            mx.rnn.FusedRNNCell(50, num_layers=3, mode='lstm',
+                               prefix='rnn_', dropout=0.5))
+
+    inputs = [mx.sym.Variable('rnn_t%d_data'%i) for i in range(2)]
+    outputs, _ = cell.unroll(2, inputs, merge_outputs=None)
+    assert sorted(cell.params._params.keys()) == \
+           ['rnn_parameters']
+
+    args, outs, auxs = outputs.infer_shape(rnn_t0_data=(10, 50), rnn_t1_data=(10, 50))
+    assert outs == [(10, 2, 50)]
+    outputs = outputs.eval(ctx=mx.cpu(0),
+                           rnn_t0_data=mx.nd.ones((10, 50), ctx=mx.cpu(0))+5,
+                           rnn_t1_data=mx.nd.ones((10, 50), ctx=mx.cpu(0))+5,
+                           rnn_parameters=mx.nd.zeros((61200,), ctx=mx.cpu(0)))
+    expected_outputs = np.ones((10, 2, 50))+5
+    assert np.array_equal(outputs[0].asnumpy(), expected_outputs)
+
+def test_bidirectional():
+    fused = mx.rnn.FusedRNNCell(100, num_layers=2, mode='lstm', prefix='',
+            bidirectional=True)
+
+    stack = mx.rnn.SequentialRNNCell()
+    stack.add(mx.rnn.BidirectionalCell(
+                mx.rnn.LSTMCell(100, prefix='l0_'),
+                mx.rnn.LSTMCell(100, prefix='r0_'),
+                output_prefix='bi_lstm_0_'))
+    stack.add(mx.rnn.BidirectionalCell(
+                mx.rnn.LSTMCell(100, prefix='l1_'),
+                mx.rnn.LSTMCell(100, prefix='r1_'),
+                output_prefix='bi_lstm_1_'))
+
+    check_rnn_consistency(fused, stack)
+    check_rnn_consistency(stack, fused)
+
+def test_unfuse():
+    for mode in ['rnn_tanh', 'rnn_relu', 'lstm']:
+        fused = mx.rnn.FusedRNNCell(
+            100, num_layers=2, mode=mode,
+            prefix='test_%s'%mode,
+            bidirectional=True,
+            dropout=0.5)
+
+        stack = fused.unfuse()
+
+        check_rnn_consistency(fused, stack)
+        check_rnn_consistency(stack, fused)
 
 ###################################
 # GRU NOT SUPPORTED
